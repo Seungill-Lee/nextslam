0e28b584c3565be3dd8cfefc823228224ad57ba3
ever9415 <ever9415@mvint.co.kr>
2024-04-12 01:28:57 +0900

    사진 구하기 어렵다~!

--- app/character/page.js
+++ app/character/page.js
@@ -2,13 +2,13 @@ import Image from "next/image";
 import scss from "./page.module.scss";
 
 export const metadata = {
-  title: "등장인물 소개 | Next Slam",
+  title: "등장인물 | Next Slam",
 };
 
 export default function Character() {
   return (
     <>
-        <h2>케릭터</h2>
+        <h2>등장인물</h2>
         <p>케릭터 입니다.</p>
     </>
   );
