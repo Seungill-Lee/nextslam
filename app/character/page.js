e792ad6240b038f8a837fbf7fdeea186d2279038
ever9415 <ever9415@mvint.co.kr>
2024-04-13 03:27:23 +0900

    케릭터소개 고민되네 ㅠ

--- app/character/page.js
+++ app/character/page.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Image from "next/image";
 import scss from "./page.module.scss";
 
@@ -6,10 +7,23 @@ export const metadata = {
 };
 
 export default function Character() {
-  return (
-    <>
-        <h2>등장인물</h2>
-        <p>케릭터 입니다.</p>
-    </>
-  );
+    return (
+        <article id={scss.character}>
+            <h2>등장인물</h2>
+            <ul className={scss.team_member}>
+                <li>
+                    <Link href="./shohoku"><Image src="/images/team/photo_sdt_shohoku.jpg" alt="상북" className="tm_photo" width={378} height={560} /></Link>
+                </li>
+                <li>
+                    <Link href="./ryonan"><Image src="/images/team/photo_sdt_ryonan.jpg" alt="능남" className="tm_photo" width={403} height={596} /></Link>
+                </li>
+                <li>
+                    <Link href="./shoyo"><Image src="/images/team/photo_sdt_shoyo.jpg" alt="상양" className="tm_photo" width={403} height={596} /></Link>
+                </li>
+                <li>
+                    <Link href="./kainan"><Image src="/images/team/photo_sdt_kainan.jpg" alt="해남" className="tm_photo" width={401} height={593} /></Link>
+                </li>
+            </ul>
+        </article>
+    );
 }
