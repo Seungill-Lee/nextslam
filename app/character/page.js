d8fdaa41a492b6fc0e31044a5a4ff9a1c307b6a2
ever9415 <ever9415@mvint.co.kr>
2024-04-13 13:20:55 +0900

    작업 열심히 하는 중... 자료 구하는게 힘들다.

--- app/character/page.js
+++ app/character/page.js
@@ -8,20 +8,40 @@ export const metadata = {
 
 export default function Character() {
     return (
-        <article id={scss.character}>
+        <article id={scss.team}>
             <h2>등장인물</h2>
-            <ul className={scss.team_member}>
+            <ul className={scss.tm_list}>
                 <li>
-                    <Link href="./shohoku"><Image src="/images/team/photo_sdt_shohoku.jpg" alt="상북" className="tm_photo" width={378} height={560} /></Link>
+                    <Link href="./shohoku">
+                        <figure>
+                            <Image src="/images/team/photo_sdt_shohoku.jpg" alt="상북" className="tm_photo" width={378} height={560} />
+                            <figcaption></figcaption>
+                        </figure>
+                    </Link>
                 </li>
                 <li>
-                    <Link href="./ryonan"><Image src="/images/team/photo_sdt_ryonan.jpg" alt="능남" className="tm_photo" width={403} height={596} /></Link>
+                    <Link href="./ryonan">
+                        <figure>
+                            <Image src="/images/team/photo_sdt_ryonan.jpg" alt="능남" className="tm_photo" width={403} height={596} />
+                            <figcaption></figcaption>
+                        </figure>
+                    </Link>
                 </li>
                 <li>
-                    <Link href="./shoyo"><Image src="/images/team/photo_sdt_shoyo.jpg" alt="상양" className="tm_photo" width={403} height={596} /></Link>
+                    <Link href="./shoyo">
+                        <figure>
+                            <Image src="/images/team/photo_sdt_shoyo.jpg" alt="상양" className="tm_photo" width={403} height={596} />
+                            <figcaption></figcaption>
+                        </figure>
+                    </Link>
                 </li>
                 <li>
-                    <Link href="./kainan"><Image src="/images/team/photo_sdt_kainan.jpg" alt="해남" className="tm_photo" width={401} height={593} /></Link>
+                    <Link href="./kainan">
+                        <figure>
+                            <Image src="/images/team/photo_sdt_kainan.jpg" alt="해남" className="tm_photo" width={401} height={593} />
+                            <figcaption></figcaption>
+                        </figure>
+                    </Link>
                 </li>
             </ul>
         </article>
