d4021ac665b0c0ac9f746a69c9029a0e4bbbafba
ever9415 <ever9415@mvint.co.kr>
2024-04-13 19:08:40 +0900

    컴포넌트 만들기 진행 중

--- app/character/page.js
+++ app/character/page.js
@@ -8,42 +8,9 @@ export const metadata = {
 
 export default function Character() {
     return (
-        <article id={scss.team}>
+        <article id={scss.charater}>
             <h2>등장인물</h2>
-            <ul className={scss.tm_list}>
-                <li>
-                    <Link href="./shohoku">
-                        <figure>
-                            <Image src="/images/team/photo_sdt_shohoku.jpg" alt="상북" className="tm_photo" width={378} height={560} />
-                            <figcaption></figcaption>
-                        </figure>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="./ryonan">
-                        <figure>
-                            <Image src="/images/team/photo_sdt_ryonan.jpg" alt="능남" className="tm_photo" width={403} height={596} />
-                            <figcaption></figcaption>
-                        </figure>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="./shoyo">
-                        <figure>
-                            <Image src="/images/team/photo_sdt_shoyo.jpg" alt="상양" className="tm_photo" width={403} height={596} />
-                            <figcaption></figcaption>
-                        </figure>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="./kainan">
-                        <figure>
-                            <Image src="/images/team/photo_sdt_kainan.jpg" alt="해남" className="tm_photo" width={401} height={593} />
-                            <figcaption></figcaption>
-                        </figure>
-                    </Link>
-                </li>
-            </ul>
+            
         </article>
     );
 }
