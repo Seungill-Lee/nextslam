63359a92371c44ec63a1bcdf442394ced68f01e1
ever9415 <ever9415@mvint.co.kr>
2024-04-24 06:59:34 +0900

    아이콘 다 구했다.

--- components/soundtrack/bpBtnSet.js
+++ components/soundtrack/bpBtnSet.js
@@ -28,14 +28,23 @@ const btnNext = (props) => (
     </svg>
 );
 
+const btnRepeat = (props) => (
+    <svg width="40px" height="40px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path d="M6 4h15a1 1 0 0 1 1 1v7h-2V6H6v3L1 5l5-4v3zm12 16H3a1 1 0 0 1-1-1v-7h2v6h14v-3l5 4-5 4v-3z" />
+    </svg>
+);
+
 const btnPlaylist = (props) => (
     <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...props}>
         <path d="M16 17a3 3 0 0 1-3 3h-2a3 3 0 0 1 0-6h2a3 3 0 0 1 1 .17V1l6-1v4l-4 .67V17zM0 3h12v2H0V3zm0 4h12v2H0V7zm0 4h12v2H0v-2zm0 4h6v2H0v-2z" />
     </svg>
 );
 
+const btnClose = (props) => (
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" {...props}>
+        <polygon points="20.3,21.7 12,13.4 3.7,21.7 2.3,20.3 10.6,12 2.3,3.7 3.7,2.3 12,10.6 20.3,2.3 21.7,3.7 13.4,12 21.7,20.3" />
+    </svg>
+);
 
 
-
-
-export {btnPlay, btnPause, btnPrev, btnNext, btnPlaylist};
+export {btnPlay, btnPause, btnPrev, btnNext, btnRepeat, btnPlaylist, btnClose};
