917595509e581886f1f4fd70dcd5dc8ed6bbf005
ever9415 <ever9415@mvint.co.kr>
2024-04-30 00:09:48 +0900

    오늘도 많이 했네.. 일찍 잔다고 해놓고.. 지금이라도 얼릉 자라.

--- app/layout.js
+++ app/layout.js
@@ -17,6 +17,13 @@ export const metadata = {
     description: "Next.js로 제작된 슬램덩크 팬사이트",
 };
 
+export const viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false
+}
+
 export default function RootLayout({ children }) {
     return (
         <html lang="ko">
