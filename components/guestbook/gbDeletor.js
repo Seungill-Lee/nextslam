e8960151b5078314665bc3fe904507938376cf0a
ever9415 <ever9415@mvint.co.kr>
2024-05-05 18:18:59 +0900

    후후..... 생각보다 빠르게 잘 끝났다. 밥먹자~!!

--- components/guestbook/gbDeletor.js
+++ components/guestbook/gbDeletor.js
@@ -19,37 +19,35 @@ export default function GbDeletor(props) {
     },[])
 
     return(
-        <div className={scss.gb_deletor}>
+        <form className={scss.gb_deletor} onSubmit={e => {
+            e.preventDefault();
+
+            const gbDeleteform = e.target;
+
+            if(orgPassword != gbPassword) {
+                alert("비밀번호가 틀렸어요")
+                setGbPassword("");
+                gbDeleteform.password.focus();
+                return false;
+            }
+
+            const options = {
+                method: "DELETE"
+            }
+            console.log(gb.id)
+            
+            fetch(`http://localhost:9999/guestbook/${gb.id}`,options).then(resp => resp.json()).then(result => {
+                router.refresh();
+            })
+        }}>
             <p className={scss.msg}>정말 삭제하시겠습니까??</p>
-            <form onSubmit={e => {
-                e.preventDefault();
-
-                const gbDeleteform = e.target;
-
-                if(orgPassword != gbPassword) {
-                    alert("비밀번호가 틀렸어요")
-                    setGbPassword("");
-                    gbDeleteform.password.focus();
-                    return false;
-                }
-
-                const options = {
-                    method: "DELETE"
-                }
-                console.log(gb.id)
-                
-                fetch(`http://localhost:9999/guestbook/${gb.id}`,options).then(resp => resp.json()).then(result => {
-                    router.refresh();
-                })
-            }}>
-                <div className={scss.field}>
-                    <input type="password" name="password" placeholder="비빌번호 확인" title="해당 게시물의 비밀번호를 입력해주세요." value={gbPassword || ""} onChange={(e) => setGbPassword(e.target.value)} />
-                </div>
-                <div className={scss.btn_set}>
-                    <button type="button" onClick={() => `${setGbId(gb.id)} ${setGbMode("GET")}`}>돌아가기</button>
-                    <button type="submit">삭제하기</button>
-                </div>
-            </form>
-        </div>
+            <div className={scss.field}>
+                <input type="password" name="password" placeholder="비빌번호 확인" title="해당 게시물의 비밀번호를 입력해주세요." value={gbPassword || ""} onChange={(e) => setGbPassword(e.target.value)} />
+            </div>
+            <div className={scss.btn_delete}>
+                <button type="button" onClick={() => `${setGbId(gb.id)} ${setGbMode("GET")}`}>돌아가기</button>
+                <button type="submit">삭제하기</button>
+            </div>
+        </form>
     )
 }
