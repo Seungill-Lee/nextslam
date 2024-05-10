a447950677875d89d61e61edd545a10ff41a3668
ever9415 <ever9415@mvint.co.kr>
2024-05-11 02:17:07 +0900

    아오... 힘들다... 삽질의 연속

--- components/soundtrack/stDetail.js
+++ components/soundtrack/stDetail.js
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 import { useRecoilValue } from "recoil";
 import { bgmPlayerID } from "../atom.js"
 import scss from "./stDetail.module.scss";
@@ -10,25 +11,43 @@ import EmptyCover from "./emptyCover.js";
 
 export default function StDetail() {
     const playID = useRecoilValue(bgmPlayerID)
+    const stDetail = useRef();
+    const detailInner = useRef();
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if(!stDetail.current) return false;
+
+            let dTop = stDetail.current.getBoundingClientRect().y;
+        
+            if(dTop <= 0) {
+                detailInner.current.style.transform = "translateY("+Math.abs(dTop)+"px)"
+            } else {
+                detailInner.current.style.transform = "translateY(0px)"
+            }
+        })
+    },[])
 
     return (
-        <div className={`${scss.st_detail} ${playID > 0 ? scss.active : ""}`} style={{"background":playID > 0 && data[playID-1].albumBgInfo ? data[playID-1].albumBgInfo : ""}}>
-            <div className={scss.cover}>
-                {playID > 0 && data[playID-1].coverImgSrc ?
-                    <Image src={data[playID-1].coverImgSrc} alt={data[playID-1].albumName ? data[playID-1].albumName : ""} width={500} height={500} className={scss.ac_thumbnail} /> : <EmptyCover className={scss.empty} />
-                }
-            </div>
-            <div  className={scss.info}>
-                {playID > 0 ? 
-                    <ul style={{"color":playID > 0 && data[playID-1].albumTxtColor ? data[playID-1].albumTxtColor : ""}}>
-                        {data[playID-1].title ? <li><span className={scss.category}>제목:</span><span className={scss.value}>{data[playID-1].title}</span></li> : ""}
-                        {data[playID-1].artist ? <li><span className={scss.category}>아티스트:</span><span className={scss.value}>{data[playID-1].artist}</span></li> : ""}
-                        {data[playID-1].albumName ? <li><span className={scss.category}>앨범명:</span><span className={scss.value}>{data[playID-1].albumName}</span></li> : ""}
-                        {data[playID-1].time ? <li><span className={scss.category}>재생시간:</span><span className={scss.value}>{data[playID-1].time}</span></li> : ""}
-                    </ul>
-                    : 
-                    <p className={scss.not_connect}>노래를 선택해주세요.</p>
-                }
+        <div className={scss.st_detail} style={{"background":playID > 0 && data[playID-1].albumBgInfo ? data[playID-1].albumBgInfo : ""}} ref={stDetail}>
+            <div className={scss.detail_inner} ref={detailInner}>
+                <div className={scss.cover}>
+                    {playID > 0 && data[playID-1].coverImgSrc ?
+                        <Image src={data[playID-1].coverImgSrc} alt={data[playID-1].albumName ? data[playID-1].albumName : ""} width={500} height={500} className={scss.ac_thumbnail} /> : <EmptyCover className={scss.empty} />
+                    }
+                </div>
+                <div  className={scss.info}>
+                    {playID > 0 ? 
+                        <ul style={{"color":playID > 0 && data[playID-1].albumTxtColor ? data[playID-1].albumTxtColor : ""}}>
+                            {data[playID-1].title ? <li><span className={scss.category}>제목:</span><span className={scss.value}>{data[playID-1].title}</span></li> : ""}
+                            {data[playID-1].artist ? <li><span className={scss.category}>아티스트:</span><span className={scss.value}>{data[playID-1].artist}</span></li> : ""}
+                            {data[playID-1].albumName ? <li><span className={scss.category}>앨범명:</span><span className={scss.value}>{data[playID-1].albumName}</span></li> : ""}
+                            {data[playID-1].time ? <li><span className={scss.category}>재생시간:</span><span className={scss.value}>{data[playID-1].time}</span></li> : ""}
+                        </ul>
+                        : 
+                        <p className={scss.not_connect}>노래를 선택해주세요.</p>
+                    }
+                </div>
             </div>
         </div>
     )
