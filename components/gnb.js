bbe35e497abbfd58ad7faedbd65de19722e26e29
ever9415 <ever9415@mvint.co.kr>
2024-05-12 12:22:12 +0900

    GNB 거의다 짰다. 밥 먹고와서 본격적으로 하자

--- components/gnb.js
+++ components/gnb.js
@@ -3,6 +3,7 @@
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import scss from "./gnb.module.scss";
+import { useState } from 'react';
 
 const menuData = [
     {name: "Home", path: "/"},
@@ -14,18 +15,28 @@ const menuData = [
 
 export default function GNB() {
     const pathname = usePathname()
+    const [gnbOpen,stateGnbOepn] = useState(false)
 
     return (
-        <nav id={scss.gnb}>
-            <ul>
-                {
-                    menuData.map((a, i) => {
-                        return (
-                            <li key={i}><Link href={a.path} className={pathname == a.path || pathname.indexOf(a.path+"/") > -1 ? scss.active : ''}>{a.name}</Link></li>
-                        )
-                    })
-                }
-            </ul>
-        </nav>
+        <>
+            <button type="button" id={scss.btn_gnb} className={gnbOpen ? scss.on : ""} onClick={() => !gnbOpen ? stateGnbOepn(true) : stateGnbOepn(false)}>
+                <span className={scss.bar}></span>
+                <span className={scss.bar}></span>
+                <span className={scss.bar}></span>
+                <span className={scss.txt}>Gnb Open</span>
+            </button>
+            <nav id={scss.gnb} className={gnbOpen ? scss.on : ""}>
+                <ul>
+                    {
+                        menuData.map((a, i) => {
+                            return (
+                                <li key={i}><Link href={a.path} className={pathname == a.path || pathname.indexOf(a.path+"/") > -1 ? scss.active : ''} onClick={() => gnbOpen ? stateGnbOepn(false) : ""}>{a.name}</Link></li>
+                            )
+                        })
+                    }
+                </ul>
+            </nav>
+            <div id={scss.bg_modal_gnb} className={gnbOpen ? scss.on : ""}></div>
+        </>
     );
 }
